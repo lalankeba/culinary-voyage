01d39ac696515a6a36b6ee463b09d8086751a412
lalankeba <lalankeba@gmail.com>
2024-03-19 12:42:17 +0200

    adding initial files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='app'>
+        <div className='container'>
+          <h1>Culinary Voyage</h1>
+          <p>Discover global recipes and cocktails</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
