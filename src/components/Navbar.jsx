b78f81568940ffd0b589ecb5bc70ea43ad451b18
lalankeba <lalankeba@gmail.com>
2024-04-08 17:05:18 +0530

    updating styling of navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Navbar.css';
 
 export const Navbar = () => {
   return (
     <nav>
         <div className="navbar">
-            <h1>Culinary Voyage</h1>
+            <h1><Link to="/">Culinary Voyage</Link></h1>
             <div className='links'>
-              <NavLink to="/">Meals</NavLink>
+              <NavLink to="/meals">Meals</NavLink>
               <NavLink to="cocktails">Cocktails</NavLink>
             </div>
         </div>
